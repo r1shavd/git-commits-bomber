ef764f4a02b2d95326465b25a95f8b15c3aa0753
Rishav Das <rdofficial192@gmail.com>
2021-05-04 08:02:36 +0530

    Deleted the option to initialize a GIT repo, added a new feature/option of empty mass commits with fixed amount + infinite amount options, changed the code structure a bit, changed the way of printing the options menu.

--- script.sh
+++ script.sh
@@ -3,7 +3,9 @@
 # Author : Rishav Das
 #
 
-function commitBomber {
+# First, we define all the functions that are used in this script
+
+function commit {
 	# The function to do a commit on the current folder (GIT Repository). The function just does 1 commit, and we can use this function in a loop in order to make mass commits.
 
 	commitMessage="Mass commit on - "$( date )
@@ -11,61 +13,26 @@ function commitBomber {
 	command=$( git commit -m "${commitMessage}" )
 }
 
-function setRepository {
-	# The function to set a repository with README.md and initial commits and all
+function emptyCommit {
+	# The function to an empty commit on the current folder (GIT repository). The function just does 1 commit, and we can call this function in a loop in order to do a series of empty commits or to say in other words, we can do mass commits.
 
-	# Getting the user entered parameter for the repository name
-	repositoryName=$1
-
-	# Writting the README.md documentation file
-	echo -e "# ${repositoryName}
-
-The documentation yet to be updated." > README.md
-
-	# Doing the initial commit for the current repository
-	command=$( git init )
-	command=$( git add --a )
-	command=$( git commit -m "Initial commit")
-	command=$( git branch -M main )
-
-	# Checking if any remote address for the repository is provided by the user, the place where our repository is to be hosted.
-	remoteOrigin=$2
-	if [[ ${#remoteOrigin} < 8 ]]; then
-		# If the user entered remote origin is less than 8 characters, then we term it as invalid and pass the process without completing it
-
-		echo -e "INITIAL COMMIT DONE...[!]"
-	else
-		# If the user entered remote origin is more than or equal to 8 characters, then we continue the process to add a remote origin to the repository
-
-		command=$( git remote add origin ${remoteOrigin} )
-		command=$( git push origin main )
-		echo -e "INITIAL COMMIT DONE...[!]"
-		echo -e "REMOTE ORIGIN ADDED, AND COMMIT PUSHED...[!]"
-	fi
+	command=$( git commit -m "Empty mass commit on - $( date )" --allow-empty )
 }
 
 # The main script starts here
 
 # Printing the main menu to the user and asking to enter an option
 printf "Choose any of the options below :\n\n1. Initialize a GIT repository\n2. Mass commit bombing [default] \n3. Fixed file mass commit bombing\n4. Mass commit bombing with reseting the file [default]\n5. Fixed file mass commit bombing with reseting the file\n0. Exit\n\n"
+echo -e "Choose any of the options below :"
+echo -e "[1] Default mass commits bombing"
+echo -e "[2] Fixed file mass commits bombing"
+echo -e "[3] Default mass commits bombing with resetting the file every commit"
+echo -e "[4] Fixed file mass commits bombing with resetting the file every commit"
+echo -e "[5] Mass empty commits\n"
 read -p "Enter your choice : " choice
 
 # Checking the user entered option
 if [[ ${choice} == "1" ]]; then
-	# If the user entered the option to initialize a new GIT repository, then we continue to complete the task
-
-	# Displaying some warning messages to the user
-	clear
-	echo -e "\nSome points to be noted before proceeding :\n1. Make sure that GIT software is installed properly on the system you are currently operating on.\n2. Make sure that you are currently in the folder that you want to make a GIT repository, otherwise wrong folders might be created as the GIT repositories.\n4. If you enter the remote origin, check after the process to make sure remote origin is pushed. Many times it fails to do so.\n"
-
-	# Asking the user for the repository name to be initialized
-	read -p "Enter the repository name : " $repositoryName
-
-	# Asking for the remote origin for the GIT repository
-	read -p "Enter the remote origin for the repository [ leave blank if none ] : " $remoteOrigin
-
-	setRepository "${repositoryName}" "${remoteOrigin}"
-elif [[ ${choice} == "2" ]]; then
 	# If the user entered the option to mass commit an existing GIT repository, then we continue to complete the task
 
 	# Displaying some warning messages to the user
@@ -85,12 +52,12 @@ elif [[ ${choice} == "2" ]]; then
 		echo "Change for the commit-${commitNumber} on $( date ). This commit was made for the mass commit bombing started on ${startedOn}" >> test.txt
 
 		# Calling the commits bomber function in order to do a commit with currently made changes
-		commitBomber
+		commit
 		echo -e "[!] Commit-${commitNumber} made successfully"
 	done
 
 	echo -e "\nFINISHED COMMITS BOMBING...[!]"
-elif [[ ${choice} == "3" ]]; then
+elif [[ ${choice} == "2" ]]; then
 	# If the user entered the option to mass commit an existing GIT repository with changing just a fixed file, then we continue to complete the task
 
 	# Displaying some warning messages to the user
@@ -112,12 +79,12 @@ elif [[ ${choice} == "3" ]]; then
 		echo "Change for the commit-${commitNumber} on $( date ). This commit was made for the mass commit bombing started on ${startedOn}" >> $filename
 
 		# Calling the commits bomber function in order to do a commit with currently made changes
-		commitBomber
+		commit
 		echo -e "[!] Commit-${commitNumber} made successfully"
 	done
 
 	echo -e "\nFINISHED COMMITS BOMBING...[!]"
-elif [[ ${choice} == "4" ]]; then
+elif [[ ${choice} == "3" ]]; then
 	# If the user choosed the option to mass commit an existing GIT repository but with reseting the file at every change, then we continue to complete the task
 
 	# Displaying some warning messages to the user
@@ -137,12 +104,12 @@ elif [[ ${choice} == "4" ]]; then
 		echo "Change for the commit-${commitNumber} on $( date ). This commit was made for the mass commit bombing started on ${startedOn}. Mass commit mode : Reseting the file on every change (commit)." > test.txt
 
 		# Calling the commits bomber function in order to do a commit with currently made changes
-		commitBomber
+		commit
 		echo -e "[!] Commit-${commitNumber} made successfully"
 	done
 
 	echo -e "\nFINISHED COMMITS BOMBING...[!]"
-elif [[ ${choice} == "5" ]]; then
+elif [[ ${choice} == "4" ]]; then
 	# If the user choosed the option to mass commit an existing GIT repository but with reseting the user specified file at every change, then we continue to complete the task
 
 	# Displaying some warning messages to the user
@@ -164,9 +131,43 @@ elif [[ ${choice} == "5" ]]; then
 		echo "Change for the commit-${commitNumber} on $( date ). This commit was made for the mass commit bombing started on ${startedOn}. Mass commit mode : Reseting the file on every change (commit)." > $filename
 
 		# Calling the commits bomber function in order to do a commit with currently made changes
-		commitBomber
+		commit
 		echo -e "[!] Commit-${commitNumber} made successfully"
 	done
+
+	echo -e "\nFINISHED COMMITS BOMBING...[!]"
+elif [[ $choice == "5" ]]; then
+	# If the user chooses the option to do mass empty commits, then we continue to complete the task
+
+	# Displaying some warning messages to the user
+	clear
+	echo -e "\nSome points to be noted before proceeding : \n1. Make sure the GIT software is installed properly on the system you are currently operating on.\n2. Make sure you are in the folder that you want to mass commit bomb on, otherwise wrong folders will be mass bombed.\n"
+
+	# Asking the user for the amounts of commits to be done
+	read -p "Enter the amount of commits (0 for inifinite) : " amountOfCommits
+
+	# Checking wheter the user wants infinite commits or not
+	if [[ $amountOfCommits == "0" ]]; then
+		# If the user chooses to do infinite commits, then we continue to do so
+
+		commitNumber=0
+		while true; do
+			# Using an while..true loop for doing an infinite loop
+
+			# Calling the empty commit function
+			emptyCommit
+			commitNumber=$(($commitNumber+1))
+			echo -e "[!] Empty commit-${commitNumber} made successfully"
+		done
+	else
+		# If the user enters any other value other than 0, then we continue
+
+		for commitNumber in $( seq 1 ${amountOfCommits} ); do
+			# Calling the empty commit function
+			emptyCommit
+			echo -e "[!] Empty commit-${commitNumber} made successfully"
+		done
+	fi
 
 	echo -e "\nFINISHED COMMITS BOMBING...[!]"
 elif [[ ${choice} == "0" ]]; then
